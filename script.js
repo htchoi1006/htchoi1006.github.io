01736a39e5c443a914412ab937e2129497a69ae1
htchoi1006 <htchoi1006@naver.com>
2021-07-02 16:08:23 +0900

    목차 하이라이트 수정

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var lastId,
-    topMenu = $("#sideNav-li"),
+    topMenu = $("#sideNav-ul"),
     topMenuHeight = topMenu.outerHeight() + 1,
     menuItems = topMenu.find("a"),
     scrollItems = menuItems.map(function (){
@@ -10,32 +10,25 @@ var lastId,
         }
     });
 
+// Bind to scroll
+$(window).scroll(function(){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop()+topMenuHeight;
 
-menuItems.click(function (e){
-    var href = $(this).attr("href"),
-        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-    $('html, body').stop().animate({
-        scrollTop: offsetTop
-    }, 850);
-    e.preventDefault();
-});
-
-
-$(window).scroll(function (){
-    var fromTop = $(this).scrollTop() + topMenuHeight;
-
-    var cur = scrollItems.map(function (){
+    // Get id of current scroll item
+    var cur = scrollItems.map(function(){
         if ($(this).offset().top < fromTop)
             return this;
     });
-
-    cur = cur[cur.length - 1];
+    // Get the id of the current element
+    cur = cur[cur.length-1];
     var id = cur && cur.length ? cur[0].id : "";
 
-    if (lastId !== id){
+    if (lastId !== id) {
         lastId = id;
+        // Set/remove active class
         menuItems
             .parent().removeClass("active")
-            .end().filter("[href=#" + id + "]").parent().addClass("active");
+            .end().filter("[href=#"+id+"]").parent().addClass("active");
     }
 });
