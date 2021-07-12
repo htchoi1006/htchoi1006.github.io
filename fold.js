acba3510e71868c139f64e820104635ecb85a058
htchoi1006 <htchoi1006@naver.com>
2021-07-12 20:56:57 +0900

    1차 완성본

--- fold.js
+++ fold.js
@@ -1,91 +1,106 @@
 // var bDisplay = true;
 function doDisplay(){
     var con = document.getElementById("projects-content-div1-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div1-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div1-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
+
 function doDisplay2(){
     var con = document.getElementById("projects-content-div2-folded");
-    // if(con.style.display=='block'){
-        // con.style.display = 'none';
-    if(con.style.height == '70px'){
-        con.style.height = '0px';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div2-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        // con.style.display = 'block';
-        con.style.height = '70px';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div2-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
+
 function doDisplay3(){
     var con = document.getElementById("projects-content-div3-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div3-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div3-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
 function doDisplay4(){
     var con = document.getElementById("projects-content-div4-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div4-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div4-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
 function doDisplay5(){
     var con = document.getElementById("projects-content-div5-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div5-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div5-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
 function doDisplay6(){
     var con = document.getElementById("projects-content-div6-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div6-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div6-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
 function doDisplay7(){
     var con = document.getElementById("projects-content-div7-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div7-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div7-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
 
 function doDisplay8(){
     var con = document.getElementById("projects-content-div8-folded");
-    if(con.style.display=='block'){
-        con.style.display = 'none';
+    if(con.style.maxHeight == '70px'){
+        con.style.maxHeight = '0px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div8-icon').innerHTML = '<span class="material-icons">arrow_right</span>'
     }else{
-        con.style.display = 'block';
+        con.style.maxHeight = '70px';
+        con.style.transition = 'transition: max-height 0.1s ease-out;'
         document.getElementById('projects-content-div8-icon').innerHTML = '<span class="material-icons">arrow_drop_down</span>'
     }
 }
